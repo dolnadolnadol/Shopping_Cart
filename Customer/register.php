5d98f9c0922dd5ba7eac3fc9ce953cd0d63b618e
Kittilphan <puma_lol@outlook.com>
2024-03-01 11:24:51 +0700

    Sign UP security+

--- Customer/register.php
+++ Customer/register.php
@@ -54,7 +54,7 @@
             cursor: pointer;
             width: 150px;
         }
-
+        
         .radio-label {
             display: flex;
             align-items: center;
@@ -81,40 +81,71 @@
         .main-container {
             margin-top:-50px;
         }
+        
+        .error-message {
+            color: red;
+            font-size: 14px;
+            margin-top: -10px;
+            margin-bottom: 10px;
+        }
     </style>
 </head>
 <body>
     <?php include('./component/backLogin.php')?>
     <div class="main-container">
-    <div class="font-login">
-        <p>Register</p>
-    </div>
-    <div class="body-container">
-        <div class="container">
-            <form method="post" action="registerProcess.php">
-                <label for="frist name">Frist name</label>
-                <input type="text" id="frist name" name="fname" required>
-                <label for="last name">Last name</label>
-                <input type="text" id="last name" name="lname" required>
-
-                <label for="username">Username</label>
-                <input type="text" id="username" name="username" required>
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" required>
-                <label for="tel">Tel</label>
-                <input type="tel" id="tel" name="tel" required>
-                
-                <label class="radio-label">
-                    <input type="radio" class="radio-input" name="sex" value="M"> Male
-                </label>   
-                <label class="radio-label">
-                    <input type="radio" class="radio-input" name="sex" value="F"> Female
-                </label>
-
-                <input type="submit" value="ลงทะเบียน">
-            </form>          
+        <div class="font-login">
+            <p>Register</p>
+        </div>
+        <div class="body-container">
+            <div class="container">
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                    <label for="fname">First name</label>
+                    <input type="text" id="fname" name="fname" required>
+                    <label for="lname">Last name</label>
+                    <input type="text" id="lname" name="lname" required>
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" required>
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" required>
+                    <p id ="password-error" class="error-message"></p>
+                    <label for="tel">Tel</label>
+                    <input type="tel" id="tel" name="tel" required>
+                    <label class="radio-label">
+                        <input type="radio" class="radio-input" name="sex" value="M" required> Male
+                    </label>   
+                    <label class="radio-label">
+                        <input type="radio" class="radio-input" name="sex" value="F"> Female
+                    </label>
+                    <input type="submit" value="ลงทะเบียน">
+                </form> 
+            </div>
         </div>
     </div>
-    </div>
+
+    <script>
+        document.getElementById('password').addEventListener('input', function() {
+            var password = this.value;
+            var passwordError = document.getElementById('password-error');
+            var specialChars = /[!@#$%^&*()\-_=+{};:,<.>]/;
+            var numericChars = /[0-9]/;
+            var alphabeticChars = /[a-z]/;
+            var alphabeticCharsUp = /[A-Z]/;
+            if (!(specialChars.test(password))) {
+                passwordError.textContent = "รหัสผ่านต้องประกอบด้วยอักษรพิเศษอย่างน้อย 1 ตัวอักษร";
+            } else if (!(alphabeticCharsUp.test(password))) {
+                passwordError.textContent = "รหัสผ่านต้องประกอบด้วยตัวอักษรตัวใหญ่อย่างน้อย 1 ตัวอักษร";
+            } else if (!(alphabeticChars.test(password))) {
+                passwordError.textContent = "รหัสผ่านต้องประกอบด้วยตัวอักษรตัวเล็กอย่างน้อย 1 ตัวอักษร";
+            } else if (!(numericChars.test(password))) {
+                passwordError.textContent = "รหัสผ่านต้องประกอบด้วยตัวเลขอย่างน้อย 1 ตัว";
+            } else if(password.length < 8){
+                passwordError.textContent = "รหัสผ่านต้องมีความยาวมากกว่า 8 ตัวอักษร";
+            } else if (password.length > 24) {
+                passwordError.textContent = "รหัสผ่านต้องมีความยาวไม่เกิน 24 ตัวอักษร";
+            } else {
+                passwordError.textContent = "";
+            }
+        });
+    </script>
 </body>
 </html>
